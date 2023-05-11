37cbb51c9ad11a1d2479d7242e1fdf53e9cf8dde
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-11 18:46:47 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsdom = require('jsdom');
+const fetch = require('node-fetch');
+async function fetchdom(url='https://github.com/zakarialaoui10'){
+  const data=await fetch(url)
+  const html=await data.text()
+  const dom= await new jsdom.JSDOM(html)
+  return dom.window
+}
