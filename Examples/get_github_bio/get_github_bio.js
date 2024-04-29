1f57f8beff0770fd615fb9a6ff6036f9f0d790f1
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-04-29 18:42:05 +0100

    .

--- Examples/get_github_bio/get_github_bio.js
+++ Examples/get_github_bio/get_github_bio.js
@@ -1,3 +1,4 @@
+import { fetchdom } from "../.."
 fetchdom('https://github.com/zakarialaoui10')
   .then(e=>e.querySelector("[data-bio-text]").textContent)
   .then(e=>console.log(e))
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const jsdom = require('jsdom');
-const fetch = require('node-fetch');
-async function fetchdom(url='https://github.com/zakarialaoui10'){
-  const data=await fetch(url)
-  const html=await data.text()
-  const dom= await new jsdom.JSDOM(html)
-  return dom.window.document
+import jsdom from 'jsdom';
+const fetchdom= async (url = 'https://github.com/zakarialaoui10', options = {})=>{
+    try {
+        const data = await fetch(url, options);
+        if (!data.ok) throw new Error(`Failed to fetch: ${data.status} - ${data.statusText}`);
+        const html = await data.text();
+        const dom = new jsdom.JSDOM(html);
+        return dom.window.document;
+    } catch (error) {
+        console.error('Error fetching or parsing HTML:', error.message);
+        return null;
+    }
 }
+
+export {
+  fetchdom
+}
+fetchdom("https://github.com/zakarialaoui10")
+ .then(e=>e.querySelector("[data-bio-text]").textContent)
+ .then(e=>console.log(e))
+ .catch(e=>console.log(e))
