bb2fe489defcd7fc4c00ea3a66e383c5dfb0b6df
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-05-10 10:37:59 +0100

    .

--- automation/index.js
+++ automation/index.js
@@ -1,10 +1,7 @@
 import path from "path" 
-import ZikoDb from "zikodb"
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
-const Config= new ZikoDb(ROOT)
 console.log({
   ROOT,
   TARGET,
-  Config
 })
