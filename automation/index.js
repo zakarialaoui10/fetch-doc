26884b7cb11df807ab0b34c2962af20eb133a72b
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-05-10 10:48:53 +0100

    .

--- automation/index.js
+++ automation/index.js
@@ -4,7 +4,7 @@ const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
 //const Config = fs.readFileSync("fetch-doc.config.js")
 console.log({
-  ROOT,
+  ROOT:process.cwd(),
   TARGET,
   ConfigPath:path.join(ROOT,"fetch-doc.config.js")
 })
