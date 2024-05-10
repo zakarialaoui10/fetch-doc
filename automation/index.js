0219b11fa0841d2a0532f28aef00212dd86aca3c
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-05-10 10:46:24 +0100

    .

--- automation/index.js
+++ automation/index.js
@@ -2,7 +2,7 @@ import path from "path"
 import fs from "fs"
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
-const Config = fs.readFileSync(path.join(ROOT,"fetch-doc.config.js"))
+const Config = fs.readFileSync("fetch-doc.config.js")
 console.log({
   ROOT,
   TARGET,
