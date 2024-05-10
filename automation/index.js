596620b1326924ca5528589a29cd8296ce529ab0
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-05-10 10:42:09 +0100

    .

--- automation/index.js
+++ automation/index.js
@@ -1,7 +1,10 @@
-import path from "path" 
+import path, { dirname } from "path"
+import fs from "fs"
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
+const Config = fs.readFileSync()
 console.log({
   ROOT,
+  dirname:path.dirname,
   TARGET,
 })
